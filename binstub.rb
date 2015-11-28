46d9599db3c76b6f0293a5c1d3cfcaf76bc6bcba
davygora <glory-gerrard@ukr.net>
2015-11-28 21:29:33 +0200

    test sorts

--- binstub.rb
+++ binstub.rb
@@ -1,18 +1,21 @@
 
 require_relative 'lib/btree'
+
 sequence = Array.new
-100.times do
-  sequence << rand(10)
+
+20.times do
+  sequence << rand(20)
 end
 
 sequence.uniq!
 
-# sequence = [4, 2, 6, 1, 6, 7]
 btree = BTree.new(sequence.shift)
 
 sequence.each do |e|
   btree.add(e)
 end
-puts sequence
-result = btree.find(9)
-puts result
+
+#result = btree.find(0)
+#puts (result ? 'yes' : 'no')
+puts btree.sort_asc
+puts btree.sort_desc
